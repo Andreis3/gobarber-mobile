97dc70e62363e2e914cda8ad6da1ebbd626dbb60
Andreis3 <andrei.as3@hotmail.com>
2020-05-06 22:56:42 -0300

    create input and button you're styled

--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -1,14 +1,30 @@
 import React from 'react';
 import { Image } from 'react-native';
 
-import { Container } from './styled';
+import Input from '../../components/input';
+import Button from '../../components/button';
 
 import logoInf from '../../assets/logo.png';
+
+import { Container, Title } from './styled';
 
 const SignIn: React.FC = () => {
   return (
     <Container>
       <Image source={logoInf} />
+
+      <Title>Faça seu Logon</Title>
+
+      <Input name="email" icon="mail" placeholder="E-mail" />
+      <Input name="password" icon="lock" placeholder="Senha" />
+
+      <Button
+        onPress={() => {
+          console.log('deu');
+        }}
+      >
+        Entrar
+      </Button>
     </Container>
   );
 };
